6c240676447c2d884754f5ac8158bec1ddb1a575
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-20 22:38:40 +0900

    fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate iron;
+extern crate staticfile;
+extern crate mount;
+
+use iron::prelude::*;
+use staticfile::Static;
+use mount::Mount;
+use std::path::Path;
+use std::env;
+
+fn get_server_port() -> u16 {
+    env::var("PORT").ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(3000)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut mount = Mount::new();
+
+    mount.mount("/", Static::new(Path::new("static/index.html")));
+
+    mount.mount("/index.js", Static::new(Path::new("static/index.js")));
+
+    Iron::new(mount).http(("0.0.0.0", get_server_port())).unwrap();
 }
